295acabad683dd6508f98f12d613e2692320ea84
rinaAr <112974283+rinaAr@users.noreply.github.com>
2024-06-25 16:16:01 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,28 +1,21 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    """
-    Encrypts plaintext using a Vigenere cipher.
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword_repeat = (keyword * (len(plaintext) // len(keyword))) + keyword[:len(plaintext) % len(keyword)]
+    for p, k in zip(plaintext, keyword_repeat):
+        if p.isalpha():
+            shift_base = 65 if p.isupper() else 97
+            shift = ord(k.upper()) - 65
+            p = chr((ord(p) - shift_base + shift) % 26 + shift_base)
+        ciphertext += p
     return ciphertext
 
-
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword_repeat = (keyword * (len(ciphertext) // len(keyword))) + keyword[:len(ciphertext) % len(keyword)]
+    for c, k in zip(ciphertext, keyword_repeat):
+        if c.isalpha():
+            shift_base = 65 if c.isupper() else 97
+            shift = ord(k.upper()) - 65
+            c = chr((ord(c) - shift_base - shift) % 26 + shift_base)
+        plaintext += c
     return plaintext
