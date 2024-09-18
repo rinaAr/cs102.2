a17a1af9469a5d7f3be10a2cda6bedcaf2aa0276
rinaAr <112974283+rinaAr@users.noreply.github.com>
2024-09-18 17:11:17 +0300

    Update life.py

--- homework04/life.py
+++ homework04/life.py
@@ -11,45 +11,35 @@ Grid = tp.List[Cells]
 
 
 class GameOfLife:
-    def __init__(
-        self,
-        size: tp.Tuple[int, int],
-        randomize: bool = True,
-        max_generations: tp.Optional[float] = float("inf"),
-    ) -> None:
-        # Размер клеточного поля
+    def __init__(self, size: tp.Tuple[int, int], randomize: bool = True, max_generations: tp.Optional[float] = float("inf")) -> None:
         self.rows, self.cols = size
-        # Предыдущее поколение клеток
         self.prev_generation = self.create_grid()
-        # Текущее поколение клеток
         self.curr_generation = self.create_grid(randomize=randomize)
-        # Максимальное число поколений
         self.max_generations = max_generations
-        # Текущее число поколений
         self.generations = 1
 
     def create_grid(self, randomize: bool = False) -> Grid:
-    """
-    Создание списка клеток.
+        """
+        Создание списка клеток.
 
-    Клетка считается живой, если ее значение равно 1, в противном случае клетка
-    считается мертвой, то есть, ее значение равно 0.
+        Клетка считается живой, если ее значение равно 1, в противном случае клетка
+        считается мертвой, то есть, ее значение равно 0.
 
-    Parameters
-    ----------
-    randomize : bool
-        Если значение истина, то создается матрица, где каждая клетка может
-        быть равновероятно живой или мертвой, иначе все клетки создаются мертвыми.
+        Parameters
+        ----------
+        randomize : bool
+            Если значение истина, то создается матрица, где каждая клетка может
+            быть равновероятно живой или мертвой, иначе все клетки создаются мертвыми.
 
-    Returns
-    ----------
-    out : Grid
-        Матрица клеток размером `cell_height` х `cell_width`.
-    """
-    if randomize:
-        return [[random.randint(0, 1) for _ in range(self.cell_width)] for _ in range(self.cell_height)]
-    else:
-        return [[0 for _ in range(self.cell_width)] for _ in range(self.cell_height)]
+        Returns
+        ----------
+        out : Grid
+            Матрица клеток размером `cell_height` х `cell_width`.
+        """
+        if randomize:
+            return [[random.randint(0, 1) for _ in range(self.cols)] for _ in range(self.rows)]
+        else:
+            return [[0 for _ in range(self.cols)] for _ in range(self.rows)]
 
 
     def get_neighbours(self, cell: Cell) -> Cells:
