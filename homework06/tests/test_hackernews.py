695b23c020f35f3a9cd91bba2c9e9e795253a88c
rinaAr <112974283+rinaAr@users.noreply.github.com>
2024-09-18 16:28:49 +0300

    Update test_hackernews.py

--- homework06/tests/test_hackernews.py
+++ homework06/tests/test_hackernews.py
@@ -2,7 +2,7 @@ import unittest
 from unittest import mock
 from unittest.mock import call
 
-from boddle import boddle
+from bottle import boddle
 from db import News
 from hackernews import add_label, classify_news, update_news
 
